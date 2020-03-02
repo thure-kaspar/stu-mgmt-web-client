cd7ab3218e5952dd1911529912113c8bc3ba9db4
LukasK-roboyor <lukas.klingebiel@web.de>
2020-03-02 15:26:43 +0100

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { environment } from "../environments/environment";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { ApiModule, BASE_PATH } from "../../api/typescript-angular-client-generated";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
 	declarations: [
@@ -13,9 +16,12 @@ import { ApiModule, BASE_PATH } from "../../api/typescript-angular-client-genera
 	imports: [
 		BrowserModule,
 		HttpClientModule,
+		ApiModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MaterialModule
 	],
 	providers: [{ provide: BASE_PATH, useValue: environment.API_BASE_PATH }],
-	bootstrap: [AppComponent],
-	exports: [ApiModule]
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
