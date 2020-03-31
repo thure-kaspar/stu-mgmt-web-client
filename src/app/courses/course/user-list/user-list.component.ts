80101b5d7193f67f8cbe07b515ab7025188c5121
LukasK-roboyor <lukas.klingebiel@web.de>
2020-03-31 16:56:10 +0200

    Implemented removing user from course

--- src/app/courses/course/user-list/user-list.component.ts
+++ src/app/courses/course/user-list/user-list.component.ts
@@ -3,6 +3,10 @@ import { CoursesService, UserDto } from "../../../../../api";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
+import { MatDialog } from "@angular/material/dialog";
+import { ConfirmDialogComponent, ConfirmDialogData } from "../../../shared/components/dialogs/confirm-dialog/confirm-dialog.dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "app-user-list",
@@ -13,14 +17,17 @@ export class UserListComponent implements OnInit {
 
 	@Input() courseId: string;
 	users: UserDto[];
-	displayedColumns: string[] = ["role", "name"];
+	displayedColumns: string[] = ["role", "name", "actions"];
 	dataSource: MatTableDataSource<UserDto>;
 	userFilter: string;
 
 	@ViewChild(MatPaginator) paginator: MatPaginator;
 	@ViewChild(MatSort) sort: MatSort;
 
-	constructor(private courseService: CoursesService) { }
+	constructor(private courseService: CoursesService,
+				public dialog: MatDialog,
+				private snackbar: MatSnackBar,
+				private router: Router) { }
 
 	ngOnInit(): void {
 		this.courseService.getUsersOfCourse(this.courseId).subscribe(
@@ -40,6 +47,47 @@ export class UserListComponent implements OnInit {
 		if (this.dataSource.paginator) {
 			this.dataSource.paginator.firstPage();
 		}
+	}
+
+	openChangeRoleDialog(user: UserDto): void {
+		//this.dialog.open()
+	}
+
+	/**
+	 * Opens the ConfirmDialog and proceeds with removal, if user confirms the action.
+	 */
+	openRemoveDialog(user: UserDto): void {
+		// Open ConfirmDialog
+		this.dialog.open<ConfirmDialogComponent, ConfirmDialogData, boolean>(ConfirmDialogComponent, {
+			data: {
+				params: [user.username, user.courseRole]
+			}
+		}).afterClosed().subscribe(
+			isConfirmed => {
+				// Check if user confirmed the action
+				if (isConfirmed) {
+					this.courseService.removeUser(this.courseId, user.id).subscribe(
+						result => {
+							if (result) {
+								// Remove removed user from user list
+								this.users = this.users.filter(u => u.username !== user.username);
+								this.refreshDataSource();
+								this.snackbar.open("User has been removed successfully.", "OK", { duration: 3000 });
+							}
+						},
+						error => {
+							console.log(error),
+							this.snackbar.open("Failed to remove the user.", "OK", { duration: 3000 });
+						}
+					);
+				}
+			}, 
+			error => console.log(error)
+		);
+	}
+
+	private refreshDataSource(): void {
+		this.dataSource = new MatTableDataSource(this.users);
 	}
 
 }
