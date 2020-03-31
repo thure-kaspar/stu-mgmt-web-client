d4ef087d72f19f61a99cbcf0a5ff410bbc8102d6
LukasK-roboyor <lukas.klingebiel@web.de>
2020-03-31 14:57:24 +0200

    Implemented opening specific tabs with fragment

--- src/app/courses/course/course/course.component.ts
+++ src/app/courses/course/course/course.component.ts
@@ -11,6 +11,18 @@ export class CourseComponent implements OnInit {
 
 	course: CourseDto;
 
+	navLinks = [
+		{ label: "Groups", path: "groups"},
+		{ label: "Users", path: "users"}
+	];
+
+	selectedTab = 0;
+	tabIndexMap = new Map([
+		["assignments", 0],
+		["groups", 1],
+		["users", 2]
+	]);
+
 	constructor(private route: ActivatedRoute,
 				private router: Router,
 				private courseService: CoursesService) { }
@@ -19,9 +31,29 @@ export class CourseComponent implements OnInit {
 		const semester = this.route.snapshot.paramMap.get("semester");
 		const name = this.route.snapshot.paramMap.get("name");
 		this.courseService.getCourseByNameAndSemester(name, semester).subscribe(
-			result => this.course = result,
+			result => {
+				this.course = result;
+				this.route.fragment.subscribe(
+					result => this.selectedTab = this.tabIndexMap.get(result),
+					error => console.log(error)
+				);
+			},
 			error => this.router.navigate(["/404"])
 		);
+	}
+
+	handleTabChange(index: number): void {
+		const tabName = this.getTabName(index);
+		this.router.navigate([], { fragment: tabName });
+	}
+
+	private getTabName(index: number): string {
+		switch (index) {
+		case 0: return "assignments";
+		case 1: return "groups";
+		case 2: return "users";
+		default: return null;
+		}
 	}
 
 }
