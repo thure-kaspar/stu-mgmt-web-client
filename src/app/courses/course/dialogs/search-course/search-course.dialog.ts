e98c867057bd155b85ef63a23481be34a126bd32
LukasK-roboyor <lukas.klingebiel@web.de>
2020-04-06 23:39:15 +0200

    Implemented generic search-course dialog

---
+++ src/app/courses/course/dialogs/search-course/search-course.dialog.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { CourseDto, CoursesService } from "../../../../../../api";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatDialogRef } from "@angular/material/dialog";
+import { SelectionModel } from "@angular/cdk/collections";
+
+@Component({
+	selector: "app-search-course",
+	templateUrl: "./search-course.dialog.html",
+	styleUrls: ["./search-course.dialog.scss"]
+})
+export class SearchCourseDialog implements OnInit {
+
+	title = "";
+	shortname: string;
+	selectedSemester = "wise1920";
+
+	courseList: CourseDto[];
+	displayedColumns: string[] = ["select", "title", "semester", "action"];
+	dataSource: MatTableDataSource<CourseDto>;
+	selection = new SelectionModel<CourseDto>(true, []);
+
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
+
+	constructor(public dialogRef: MatDialogRef<SearchCourseDialog, CourseDto[]>,
+				private courseService: CoursesService) { }
+
+	ngOnInit(): void {
+
+	}
+
+	/** Closes the dialog without returning data. */
+	onCancel(): void {
+		this.dialogRef.close();
+	}
+
+	/**Closes the dialog and returns the selected courses. */
+	onConfirm(): void {
+		this.dialogRef.close(this.selection.selected);
+	}
+
+	/** Retrieves all courses that match the specified filters and inserts them into the table. */
+	searchCourses(): void {
+		this.courseService.getCourses(this.shortname, this.selectedSemester, this.title).subscribe(
+			result => {
+				this.courseList = result;
+				this.dataSource = new MatTableDataSource(this.courseList);
+				this.dataSource.paginator = this.paginator;
+				this.dataSource.sort = this.sort;
+			},
+			error => console.log(error)
+		);
+	}
+
+	/** Whether the number of selected elements matches the total number of rows. */
+	isAllSelected(): boolean {
+		const numSelected = this.selection.selected.length;
+		const numRows = this.dataSource.data.length;
+		return numSelected === numRows;
+	}
+
+	/** Selects all rows if they are not all selected; otherwise clear selection. */
+	masterToggle(): void {
+		this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row));
+	}
+
+}
