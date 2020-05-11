3a2c5616908fe0fd53e777aa6732537358e2e801
LukasK-roboyor <lukas.klingebiel@web.de>
2020-05-11 15:32:58 +0200

    Display user's group in unique div

--- src/app/group/components/group-list/group-list.component.ts
+++ src/app/group/components/group-list/group-list.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from "@angular/material/dialog";
 import { CreateGroupDialog } from "../../dialogs/create-group/create-group.dialog";
 import { JoinGroupDialog, JoinGroupDialogData } from "../../dialogs/join-group/join-group.dialog";
 import { Router, ActivatedRoute } from "@angular/router";
+import { AuthService } from "../../../auth/services/auth.service";
 
 @Component({
 	selector: "app-group-list",
@@ -14,17 +15,29 @@ export class GroupListComponent implements OnInit {
 
 	@Input() courseId: string;
 	groups: GroupDto[];
+	groupOfUser: GroupDto;
 	groupSettings: GroupSettingsDto;
 
 	constructor(public dialog: MatDialog,
 				private groupService: GroupsService,
 				private courseConfig: CourseConfigService,
+				private authService: AuthService,
 				private router: Router,
 				private route: ActivatedRoute) { }
 
 	ngOnInit(): void {
 		this.groupService.getGroupsOfCourse(this.courseId).subscribe(
-			result => this.groups = result,
+			result => {
+				this.groups = result;
+
+				// Search groups for group containing the current user
+				this.groupOfUser = this.groups.find(g => {
+					return !!g.users.find(u => u.id === this.authService.getAuthToken().userId);
+				});
+
+				// Exclude user's group from the rest of groups
+				this.groups = this.groups.filter(g => g !== this.groupOfUser);
+			},
 			error => console.log(error)
 		);
 
