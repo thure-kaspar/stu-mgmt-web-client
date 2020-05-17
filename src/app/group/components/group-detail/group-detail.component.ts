fdb6bcda9f60f34b95cc0c1b956fa54f61f3f557
LukasK-roboyor <lukas.klingebiel@web.de>
2020-05-17 23:09:11 +0200

    Add design for group detail component

--- src/app/group/components/group-detail/group-detail.component.ts
+++ src/app/group/components/group-detail/group-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { GroupsService, GroupDto } from "../../../../../api";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { GroupsService, GroupDto, AssessmentDto, UserDto } from "../../../../../api";
 import { ActivatedRoute } from "@angular/router";
 import { AuthService } from "../../../auth/services/auth.service";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatSort } from "@angular/material/sort";
 
 @Component({
 	selector: "app-group-detail",
@@ -12,16 +14,37 @@ export class GroupDetailComponent implements OnInit {
 
 	group: GroupDto;
 
+	assessments: AssessmentDto[];
+	displayedColumns: string[] = ["name", "type", "score", "action"];
+	dataSource: MatTableDataSource<AssessmentDto>;
+
+	@ViewChild(MatSort) sort: MatSort;
+	
 	constructor(private groupService: GroupsService,
 				private authService: AuthService,
-				private route: ActivatedRoute,) { }
+				private route: ActivatedRoute) { }
 
 	ngOnInit(): void {
 		const groupId = this.route.snapshot.paramMap.get("groupId");
 		this.groupService.getGroup("java-wise1920", groupId).subscribe(
-			result => { this.group = result; },
+			result => { 
+				this.group = result;
+				this.assessments = this.group.assessments;
+				this.dataSource = new MatTableDataSource(this.assessments);
+				this.dataSource.sort = this.sort;
+			},
 			error => console.log(error)
 		);
+	}
+
+	/** TODO */
+	onPromoteToLeader(user: UserDto): void {
+
+	}
+
+	/** TODO */
+	onRemoveUser(user: UserDto): void {
+
 	}
 
 }
