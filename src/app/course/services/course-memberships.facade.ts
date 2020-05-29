45ced6f10ca6ee41e7683feb3b834d22367b046f
LukasK-roboyor <lukas.klingebiel@web.de>
2020-05-30 00:28:56 +0200

    Refactoring: Return obserables and utilize rxjs-operators

--- src/app/course/services/course-memberships.facade.ts
+++ src/app/course/services/course-memberships.facade.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, of, Observable, throwError } from "rxjs";
 import { CoursesService, UsersService, CourseDto } from "../../../../api";
 import { AuthService } from "../../auth/services/auth.service";
+import { switchMap, catchError } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class CourseMembershipsFacade {
@@ -45,34 +46,52 @@ export class CourseMembershipsFacade {
 		}
 	}
 
-	joinCourse(courseId: string, password?: string): void {
-		const userId = this.authService.getAuthToken().userId;
-
-		this.courseService.addUser({ password }, courseId, userId).subscribe(
-			async success => {
-				const courses = await this.userService.getCoursesOfUser(userId).toPromise();
-				this.coursesSubject.next(courses);
-			},
-			error => {
-				console.log(error);
-				throw new Error("Failed to join the course.");
-			}
-		);
+	/**
+	 * Adds the user to the course and updates the user's courses.
+	 * If joining fails, the error will be passed on to the caller.
+	 */
+	joinCourse(courseId: string, password?: string): Observable<void> {
+		const userId = this.getUserId();
+		return this.courseService.addUser({ password }, courseId, userId)
+			.pipe(
+				switchMap(
+					success => {
+						this.loadCoursesOfUser();
+						return of(null);
+					}
+				),
+				catchError(error => {
+					console.log(error);
+					return throwError(error);
+				})
+			);
 	}
 
-	leaveCourse(courseId: string): void {
-		const userId = this.authService.getAuthToken().userId;
+	/**
+	 * Removes the user from the course and updates the user's courses.
+	 * If leaving fails, the error will be passed on to the caller.
+	 */
+	leaveCourse(courseId: string): Observable<void> {
+		const userId = this.getUserId();
 
-		this.courseService.removeUser(courseId, userId).subscribe(
-			async success => {
-				const courses = await this.userService.getCoursesOfUser(userId).toPromise();
-				this.coursesSubject.next(courses);
-			},
-			error => {
-				console.log(error);
-				throw new Error("Failed to leave course.");
-			}
-		);
+		return this.courseService.removeUser(courseId, userId)
+			.pipe(
+				switchMap(
+					success => {
+						if (success) {
+							const courses = this.coursesSubject.getValue().filter(c => c.id !== courseId);
+							this.coursesSubject.next(courses);
+							return of(null);
+						} else {
+							return throwError(new Error("Failed to leave the course.")); // TODO: Refactor, when removeUser returns void (or error)
+						}
+					}
+				),
+				catchError(error => {
+					console.log(error);
+					return throwError(error);
+				})
+			);
 	}
 
 	private getUserId(): string {
