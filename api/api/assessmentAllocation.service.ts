da0490ae2f6585e8d9a41fe22b0bb91de5b81cb1
LukasK-roboyor <lukas.klingebiel@web.de>
2020-06-13 23:57:59 +0200

    api: Update client

--- api/api/assessmentAllocation.service.ts
+++ api/api/assessmentAllocation.service.ts
@@ -56,6 +56,57 @@ export class AssessmentAllocationService {
 
 
     /**
+     * Copy assessment allocation from another assignment
+     * Applies the allocations from another assignment to the specified assignment.
+     * @param courseId 
+     * @param assignmentId 
+     * @param existingAssignmentId 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public addAllocationsFromExistingAssignment(courseId: string, assignmentId: string, existingAssignmentId: string, observe?: 'body', reportProgress?: boolean): Observable<Array<AssessmentAllocationDto>>;
+    public addAllocationsFromExistingAssignment(courseId: string, assignmentId: string, existingAssignmentId: string, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<Array<AssessmentAllocationDto>>>;
+    public addAllocationsFromExistingAssignment(courseId: string, assignmentId: string, existingAssignmentId: string, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<Array<AssessmentAllocationDto>>>;
+    public addAllocationsFromExistingAssignment(courseId: string, assignmentId: string, existingAssignmentId: string, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        if (courseId === null || courseId === undefined) {
+            throw new Error('Required parameter courseId was null or undefined when calling addAllocationsFromExistingAssignment.');
+        }
+
+        if (assignmentId === null || assignmentId === undefined) {
+            throw new Error('Required parameter assignmentId was null or undefined when calling addAllocationsFromExistingAssignment.');
+        }
+
+        if (existingAssignmentId === null || existingAssignmentId === undefined) {
+            throw new Error('Required parameter existingAssignmentId was null or undefined when calling addAllocationsFromExistingAssignment.');
+        }
+
+        let headers = this.defaultHeaders;
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+        ];
+
+        return this.httpClient.request<Array<AssessmentAllocationDto>>('post',`${this.basePath}/courses/${encodeURIComponent(String(courseId))}/assignments/${encodeURIComponent(String(assignmentId))}/assessment-allocations/from-existing/${encodeURIComponent(String(existingAssignmentId))}`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
      * Assign assessment to evaluator
      * Maps an evaluator to a group or user. If the group or user is already assigned to another evaluator, changes the evaluator.
      * @param body 
