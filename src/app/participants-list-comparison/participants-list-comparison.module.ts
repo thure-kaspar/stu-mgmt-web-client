1b70da9099a47333003cd78ba446cb3f8d348d22
LukasK-roboyor <lukas.klingebiel@web.de>
2020-07-22 20:14:49 +0200

    Add participants list comparison component

---
+++ src/app/participants-list-comparison/participants-list-comparison.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+import { ParticipantsListComparisonRoutingModule } from "./participants-list-comparison-routing.module";
+import { ParticipantsListComparisonComponent } from "./participants-list-comparison/participants-list-comparison.component";
+
+
+
+@NgModule({
+	declarations: [ParticipantsListComparisonComponent],
+	imports: [
+		SharedModule,
+		ParticipantsListComparisonRoutingModule
+	]
+})
+export class ParticipantsListComparisonModule { }
