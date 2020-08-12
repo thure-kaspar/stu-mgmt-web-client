8594f32db93c864fd71b303c758f9e6debc29c4f
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-12 17:16:53 +0200

    Implement proper pagination and filtering

--- src/app/course/components/course-list/course-list.component.ts
+++ src/app/course/components/course-list/course-list.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { CoursesService, CourseDto } from "../../../../../api";
 import { MatTableDataSource } from "@angular/material/table";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
+import { BehaviorSubject } from "rxjs";
+import { distinctUntilChanged, map } from "rxjs/operators";
+import { CourseDto, CoursesService } from "../../../../../api";
 import { getSemester } from "../../../../../utils/helper";
+import { Paginator } from "../../../shared/paginator/paginator.component";
+
+class CourseFilter {
+	title = "";
+	shortname: string;
+	selectedSemester = getSemester();
+	
+	constructor(partial: Partial<CourseFilter>) {
+		Object.assign(this, partial);
+	}
+}
 
 @Component({
 	selector: "app-course-list",
@@ -12,33 +23,46 @@ import { getSemester } from "../../../../../utils/helper";
 })
 export class CourseListComponent implements OnInit {
 
-	title = "";
-	shortname: string;
-	selectedSemester = getSemester();
-
+	
 	courseList: CourseDto[];
 	displayedColumns: string[] = ["title", "semester"];
 	dataSource: MatTableDataSource<CourseDto>;
 
-	@ViewChild(MatPaginator) paginator: MatPaginator;
-	@ViewChild(MatSort) sort: MatSort;
+	filter = new CourseFilter({});
+	filterSubject = new BehaviorSubject(this.filter);
+
+	@ViewChild(Paginator, { static: true }) private paginator: Paginator;
 
 	constructor(private courseService: CoursesService) { }
 
 	ngOnInit(): void {
-		this.searchCourses();
+		this.filterSubject.pipe(
+			map(filter => new CourseFilter(filter)), // Create new reference (otherwise we would compare object to itself)
+			distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)) // Only reload, if filter changed
+		).subscribe(
+			filter => this.searchCourses()
+		);
 	}
 
-	searchCourses(): void {
-		this.courseService.getCourses(undefined, undefined, this.shortname, this.selectedSemester, this.title).subscribe(
-			result => {
-				this.courseList = result;
-				this.dataSource = new MatTableDataSource(this.courseList);
-				this.dataSource.paginator = this.paginator;
-				this.dataSource.sort = this.sort;
-			},
-			error => console.log(error)
-		);
+	searchCourses(triggeredByPaginator = false): void {
+		const [skip, take] = this.paginator.getSkipAndTake();
+		
+		this.courseService.getCourses(
+			skip, 
+			take, 
+			this.filter.shortname, 
+			this.filter.selectedSemester, 
+			this.filter.title
+		)
+			.subscribe(
+				result => {
+					this.courseList = result;
+					this.dataSource = new MatTableDataSource(this.courseList);
+
+					if (triggeredByPaginator) this.paginator.goToFirstPage();
+				},
+				error => console.log(error)
+			);
 	}
 
 }
