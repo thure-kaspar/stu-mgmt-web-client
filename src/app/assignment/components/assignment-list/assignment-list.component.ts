dda8f7db700a2b05f95e6cf80bee63def7f4e31f
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-15 18:27:29 +0200

    UI changes to assignments

--- src/app/assignment/components/assignment-list/assignment-list.component.ts
+++ src/app/assignment/components/assignment-list/assignment-list.component.ts
@@ -6,6 +6,7 @@ import { CourseFacade } from "../../../course/services/course.facade";
 import { ParticipantFacade } from "../../../course/services/participant.facade";
 import { CreateAssignmentDialog } from "../../dialogs/create-assignment/create-assignment.dialog";
 import { AssignmentManagementFacade } from "../../services/assignment-management.facade";
+import { Participant } from "../../../domain/participant.model";
 
 class AssignmentsStateMap {
 	inProgress: AssignmentDto[] = [];
@@ -25,15 +26,18 @@ export class AssignmentListComponent implements OnInit {
 
 	courseId: string;
 	assignments = new AssignmentsStateMap();
+	participant: Participant;
 
-	constructor(public participant: ParticipantFacade,
-				public course: CourseFacade,
+	constructor(public course: CourseFacade,
+				private participantFacade: ParticipantFacade,
 				private assignmentManagement: AssignmentManagementFacade,
 				private route: ActivatedRoute,
 				public dialog: MatDialog) { }
 
 	ngOnInit(): void {
 		this.courseId = this.route.parent.parent.snapshot.paramMap.get("courseId");
+		this.assignmentManagement.loadAssignmentsOfCourse(this.courseId);
+		this.participantFacade.p$.subscribe(p => this.participant = p);
 		this.assignmentManagement.assignments$.subscribe(
 			assignments => {
 				this.assignments.inProgress = assignments.filter(a => a.state === "IN_PROGRESS");
@@ -43,7 +47,6 @@ export class AssignmentListComponent implements OnInit {
 				this.assignments.invisible = assignments.filter(a => a.state === "INVISIBLE");
 			}
 		);
-		this.assignmentManagement.loadAssignmentsOfCourse(this.courseId);
 	}
 
 	openAddDialog(): void {
