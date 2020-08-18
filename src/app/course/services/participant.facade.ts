1dec334bb99125748afdf92cb35e0ea9a84cca9b
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-18 19:25:23 +0200

    Implement leaving group

--- src/app/course/services/participant.facade.ts
+++ src/app/course/services/participant.facade.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@angular/core";
-import { CourseParticipantsService, GroupDto, ParticipantDto } from "../../../../api";
+import { CourseParticipantsService, GroupDto, ParticipantDto, GroupsService } from "../../../../api";
 import { AuthService } from "../../auth/services/auth.service";
 import { Participant } from "../../domain/participant.model";
 import { CourseFacade } from "./course.facade";
 import { BehaviorSubject } from "rxjs";
+import { DialogService } from "../../shared/services/dialog.service";
+import { SnackbarService } from "../../shared/services/snackbar.service";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
 export class ParticipantFacade {
@@ -12,10 +15,15 @@ export class ParticipantFacade {
 	p$ = this.participantSubject.asObservable();
 
 	private userId: string;
+	private courseId: string;
 
 	constructor(private courseParticipants: CourseParticipantsService,
+				private groupService: GroupsService,
 				private courseFacade: CourseFacade,
-				private authService: AuthService) {
+				private authService: AuthService,
+				private snackbar: SnackbarService,
+				private dialogService: DialogService,
+				private router: Router) {
 		
 		this.authService.userInfo$.subscribe(info => {
 			//console.log("new UserId:", info?.userId);
@@ -35,12 +43,56 @@ export class ParticipantFacade {
 	 * the changed participant via `p$`.
 	 */
 	changeGroup(group: GroupDto): void {
+		this.setGroup(group);
+	}
+
+	/**
+	 * Opens the `ConfirmDialog` and if confirmed
+	 * causes the participant to leave the group and emits the changed participant via `p$`.
+	 */
+	leaveGroup(group: GroupDto): void {
+		this.dialogService.openConfirmDialog({
+			title: "Action.Custom.LeaveGroup",
+			params: [group.name]
+		}).subscribe(
+			confirmed => {
+				if (confirmed) {
+					this.groupService.removeUserFromGroup(this.courseId, group.id, this.userId).subscribe({
+						next: () => {
+							// Update the participant and emit new value
+							this.setGroup(undefined);
+							
+							this.snackbar.openSuccessMessage();
+							this.router.navigate(["/courses", this.courseId, "groups"]);
+						},
+						error: (error) => {
+							console.log(error);
+							this.snackbar.openErrorMessage();
+						}
+					});
+				}
+			}
+		);
+	}
+
+	/**
+	 * Reloads the current participant.
+	 */
+	reload(): void {
+		this.loadParticipant(this.courseId, this.userId);
+	}
+
+	/**
+	 * Sets the participants group to the given `group` or `undefined` and emits
+	 * the changed participant via `p$`.
+	 */
+	private setGroup(group?: GroupDto): void {
 		const participant = this.participantSubject.getValue();
 
 		const dto: ParticipantDto = {
 			...participant,
 			group: group,
-			groupId: group.id
+			groupId: group?.id
 		};
 
 		this.participantSubject.next(new Participant(dto));
@@ -57,6 +109,10 @@ export class ParticipantFacade {
 			participant => {
 				this.participantSubject.next(new Participant(participant));
 				//console.log("Current participant:", participant);
+			},
+			error => {
+				console.log(error);
+				this.participantSubject.next(undefined);
 			}
 		);
 	}
@@ -64,6 +120,7 @@ export class ParticipantFacade {
 	private loadParticipantWhenCourseLoaded(): void {
 		this.courseFacade.course$.subscribe(
 			course => {
+				this.courseId = course?.id;
 				if (course && this.userId) {
 					this.loadParticipant(course.id, this.userId);	
 				}
