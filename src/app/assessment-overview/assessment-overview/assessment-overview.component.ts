e4341c0c112fd5399bdb5ef250f807596afa569c
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-22 15:08:02 +0200

    Refactoring: Use onPush change detection strategy

--- src/app/assessment-overview/assessment-overview/assessment-overview.component.ts
+++ src/app/assessment-overview/assessment-overview/assessment-overview.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from "@angular/core";
-import { AssignmentDto } from "../../../../api";
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { getRouteParam } from "../../../../utils/helper";
 import { SelectedAssignmentFacade } from "../../assessment/services/selected-assignment.facade";
@@ -7,21 +6,18 @@ import { SelectedAssignmentFacade } from "../../assessment/services/selected-ass
 @Component({
 	selector: "app-assessment-overview",
 	templateUrl: "./assessment-overview.component.html",
-	styleUrls: ["./assessment-overview.component.scss"]
+	styleUrls: ["./assessment-overview.component.scss"],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AssessmentOverviewComponent implements OnInit {
 
-	assignment: AssignmentDto;
-
-	constructor(private selectedAssignment: SelectedAssignmentFacade,
+	constructor(public selectedAssignment: SelectedAssignmentFacade,
 				private route: ActivatedRoute) { }
 
 	ngOnInit(): void {
 		const courseId = getRouteParam("courseId", this.route);
 		const assignmentId = getRouteParam("assignmentId", this.route);
-		this.selectedAssignment.loadAssignment(courseId, assignmentId).subscribe(
-			assignment => this.assignment = assignment
-		);
+		this.selectedAssignment.loadAssignment(courseId, assignmentId);
 	}
 
 }
--- src/app/assessment/services/selected-assignment.facade.ts
+++ src/app/assessment/services/selected-assignment.facade.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
-import { Observable, BehaviorSubject } from "rxjs";
-import { AssignmentDto, AssignmentsService } from "../../../../api";
+import { BehaviorSubject } from "rxjs";
 import { tap } from "rxjs/operators";
+import { AssignmentDto, AssignmentsService } from "../../../../api";
 
 /**
  * Class that contains the currently selected assignment.
@@ -19,14 +19,13 @@ export class SelectedAssignmentFacade {
 	 * Loads the assignment. Should be triggered by the parent component that renders the `/assignments`-view.
 	 * Components that require the data can access it by subscribing to `selectedAssignment$`.
 	 */
-	loadAssignment(courseId: string, assignmentId: string): Observable<AssignmentDto> {
-		return this.assignmentService.getAssignmentById(courseId, assignmentId)
-			.pipe(
-				tap(
-					assignment => this.selectedAssignmentSubject.next(assignment),
-					error => console.log(error)
-				)
-			);
+	loadAssignment(courseId: string, assignmentId: string): void {
+		this.assignmentService.getAssignmentById(courseId, assignmentId).pipe(
+			tap(
+				assignment => this.selectedAssignmentSubject.next(assignment),
+				error => console.log(error)
+			)
+		).subscribe();
 	}
 
 	/** Clears the data managed by this class. */
