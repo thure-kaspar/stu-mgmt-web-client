a008bfa2fa478d3d8b124a6982da62529479c92d
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-22 15:07:38 +0200

    Remove overview from url

--- src/app/assessment/assessment-routing.module.ts
+++ src/app/assessment/assessment-routing.module.ts
@@ -5,8 +5,7 @@ import { Routes, RouterModule } from "@angular/router";
 const routes: Routes = [
 	{ path: "view/:assessmentId", loadChildren: () => import("../assessment-viewer/assessment-viewer.module").then(m => m.AssessmentViewerModule) },
 	{ path: "editor", loadChildren: () => import("../assessment-editor/assessment-editor.module").then(m => m.AssessmentEditorModule) },
-	{ path: "overview", loadChildren: () => import("../assessment-overview/assessment-overview.module").then(m => m.AssessmentOverviewModule) },
-	{ path: "", redirectTo: "overview", pathMatch: "full" }
+	{ path: "", loadChildren: () => import("../assessment-overview/assessment-overview.module").then(m => m.AssessmentOverviewModule) },
 ];
 
 @NgModule({
