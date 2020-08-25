f95c1a42bc9d6798f9bbe31f56d626eca56722c6
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-25 15:44:14 +0200

    Add themes and translate message and title

--- src/app/shared/services/toast.service.ts
+++ src/app/shared/services/toast.service.ts
@@ -1,13 +1,39 @@
 import { Injectable } from "@angular/core";
 import { ToastrService, IndividualConfig } from "ngx-toastr";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable({ providedIn: "root" })
 export class ToastService {
 
-	constructor(private toast: ToastrService) { }
-
+	constructor(private toast: ToastrService,
+				private translate: TranslateService) { }
+	
+	/** Displays a success-themed toast message. */			
 	success(message?: string, title?: string, override?: Partial<IndividualConfig>): void {
-		this.toast.success(message, title, override);
+		this.toast.success(this.tryGetTranslation(message), this.tryGetTranslation(title), override);
+	}
+
+	/** Displays an error-themed toast message. */
+	error(message?: string, title?: string, override?: Partial<IndividualConfig>): void {
+		this.toast.error(this.tryGetTranslation(message), this.tryGetTranslation(title), override);;
+	}
+
+	/** Displays a warning-themed toast message. */
+	warning(message?: string, title?: string, override?: Partial<IndividualConfig>): void {
+		this.toast.warning(this.tryGetTranslation(message), this.tryGetTranslation(title), override);
+	}
+
+	/** Displays an info-themed toast message. */
+	info(message?: string, title?: string, override?: Partial<IndividualConfig>): void {
+		this.toast.info(this.tryGetTranslation(message), this.tryGetTranslation(title), override);
+	}
+
+	/**
+	 * Tries to find the translation of the given `word`.
+	 * Returns `undefined` if no translation found or word was undefined.
+	 */
+	private tryGetTranslation(word?: string): string {
+		return word ? this.translate.instant(word): undefined;
 	}
 
 }
