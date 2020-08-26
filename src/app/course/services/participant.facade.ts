30fd5c4023dfd12b797608c5c4f4637ca00d273c
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-26 12:30:49 +0200

    Allow participant facade to drop current participant

--- src/app/course/services/participant.facade.ts
+++ src/app/course/services/participant.facade.ts
@@ -1,20 +1,20 @@
 import { Injectable } from "@angular/core";
-import { CourseParticipantsService, GroupDto, ParticipantDto, GroupsService } from "../../../../api";
+import { Router } from "@angular/router";
+import { BehaviorSubject } from "rxjs";
+import { CourseParticipantsService, GroupDto, GroupsService, ParticipantDto } from "../../../../api";
 import { AuthService } from "../../auth/services/auth.service";
 import { Participant } from "../../domain/participant.model";
-import { CourseFacade } from "./course.facade";
-import { BehaviorSubject } from "rxjs";
 import { DialogService } from "../../shared/services/dialog.service";
 import { SnackbarService } from "../../shared/services/snackbar.service";
-import { Router } from "@angular/router";
-import { distinctUntilChanged, filter } from "rxjs/operators";
 import { ToastService } from "../../shared/services/toast.service";
+import { CourseFacade } from "./course.facade";
+import { distinctUntilChanged } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class ParticipantFacade {
 
 	private participantSubject = new BehaviorSubject<Participant>(undefined);
-	participant$ = this.participantSubject.asObservable().pipe(filter(x => !!x));
+	participant$ = this.participantSubject.asObservable();
 
 	private userId: string;
 	private courseId: string;
@@ -124,14 +124,14 @@ export class ParticipantFacade {
 	 */
 	private loadParticipantWhenCourseLoaded(): void {
 		this.courseFacade.course$.pipe(
-			// Load if participant has not been loaded
-			// Only reload participant, if course changed
-			distinctUntilChanged((a, b) => this.participantSubject.getValue() && (a.id === b.id))
+			distinctUntilChanged((a, b) => a?.id === b?.id) // Only load when course has changed
 		).subscribe(
 			course => {
 				this.courseId = course?.id;
-				if (course && this.userId) {
+				if (course) {
 					this.loadParticipant(course.id, this.userId);	
+				} else {
+					this.participantSubject.next(undefined);
 				}
 			}
 		);
