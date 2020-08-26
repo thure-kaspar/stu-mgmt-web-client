c98af591fababfaf8997d2be579608062f45935b
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-26 22:01:19 +0200

    Display group member count

--- src/app/group/components/group-card/group-card.component.ts
+++ src/app/group/components/group-card/group-card.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Router } from "@angular/router";
+import { GroupDto, ParticipantDto } from "../../../../../api";
+import { Course } from "../../../domain/course.model";
 import { Group } from "../../../domain/group.model";
 import { Participant } from "../../../domain/participant.model";
-import { JoinGroupDialog, JoinGroupDialogData } from "../../dialogs/join-group/join-group.dialog";
-import { GroupSettingsDto, GroupDto, ParticipantDto } from "../../../../../api";
-import { DialogService } from "../../../shared/services/dialog.service";
 import { SearchParticipantDialog } from "../../../shared/components/dialogs/search-participant/search-participant.dialog";
+import { DialogService } from "../../../shared/services/dialog.service";
+import { JoinGroupDialog, JoinGroupDialogData } from "../../dialogs/join-group/join-group.dialog";
 
 @Component({
 	selector: "app-group-card",
@@ -16,9 +17,8 @@ import { SearchParticipantDialog } from "../../../shared/components/dialogs/sear
 })
 export class GroupCardComponent implements OnInit {
 
-	@Input() courseId: string;
 	@Input() group: Group;
-	@Input() groupSettings: GroupSettingsDto;
+	@Input() course: Course;
 	@Input() participant: Participant;
 
 	/** Emits a group that should be removed. */
@@ -26,11 +26,18 @@ export class GroupCardComponent implements OnInit {
 	/** Emits a `participant` and the group that the participant should be added to. */
 	@Output() onAddParticipant = new EventEmitter<{ group: GroupDto; participant: ParticipantDto }>();
 
+	cssClass: string;
+
 	constructor(private dialog: MatDialog,
 				private router: Router,
 				private dialogService: DialogService) { }
 
 	ngOnInit(): void {
+		if (this.group.members.length >= this.course.getMaxAllowedGroupSize()) {
+			this.cssClass = "text-red";
+		} else if (this.group.members.length < this.course.getMinGroupSizeRequirement()) {
+			this.cssClass = "WARNING";
+		}
 	}
 
 	/**
@@ -38,11 +45,11 @@ export class GroupCardComponent implements OnInit {
 	 * this group.
 	 */
 	openJoinGroupDialog(): void {
-		const data: JoinGroupDialogData = { courseId: this.courseId, groupId: this.group.id };
+		const data: JoinGroupDialogData = { courseId: this.course.id, groupId: this.group.id };
 		this.dialog.open<JoinGroupDialog, JoinGroupDialogData, boolean>(JoinGroupDialog, { data }).afterClosed().subscribe(
 			joined => {
 				if (joined) {
-					this.router.navigate(["/courses", this.courseId, "groups", this.group.id]);
+					this.router.navigate(["/courses", this.course.id, "groups", this.group.id]);
 				}
 			}
 		);
@@ -52,7 +59,7 @@ export class GroupCardComponent implements OnInit {
 	 * Opens the `SearchParticipantDialog` and emits the selected Participant (and Group) via `onAddParticipant`.
 	 */
 	addParticipant(): void {
-		this.dialog.open<SearchParticipantDialog, string, ParticipantDto[]>(SearchParticipantDialog, { data: this.courseId })
+		this.dialog.open<SearchParticipantDialog, string, ParticipantDto[]>(SearchParticipantDialog, { data: this.course.id })
 			.afterClosed().subscribe(
 				participants => {
 					// Emit the selected participant, if one was selected
