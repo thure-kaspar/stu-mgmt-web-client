5a329a572f2841a64ff54cbbe9812f9e66bfaad5
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-26 17:48:12 +0200

    Display loading indicator in login dialog and display error

--- src/app/auth/dialogs/login/login.dialog.ts
+++ src/app/auth/dialogs/login/login.dialog.ts
@@ -1,32 +1,49 @@
 import { Component, OnInit } from "@angular/core";
 import { MatDialogRef } from "@angular/material/dialog";
 import { AuthControllerService, AuthenticationInfoDto } from "../../../../../api_auth";
+import { UnsubscribeOnDestroy } from "../../../shared/components/unsubscribe-on-destroy.component";
 
 @Component({
 	selector: "app-login",
 	templateUrl: "./login.dialog.html",
 	styleUrls: ["./login.dialog.scss"]
 })
-export class LoginDialog implements OnInit {
+export class LoginDialog extends UnsubscribeOnDestroy implements OnInit {
 
 	username: string;
 	password: string;
 	errorMessage: string;
 
+	loading = false;
+
 	constructor(private dialog: MatDialogRef<LoginDialog, AuthenticationInfoDto>,
-				private auth: AuthControllerService) { }
+				private auth: AuthControllerService) { super(); }
 
 	ngOnInit(): void {
 	}
 
 	onLogin(): void {
-		this.auth.authenticate({ username: this.username, password: this.password }).subscribe(
+		this.loading = true;
+		this.subs.sink = this.auth.authenticate({ username: this.username, password: this.password }).subscribe(
 			result => {
+				this.loading = false;
 				this.dialog.close(result); // Return userinfo and token to calling component
 			},
 			error => {
 				console.log(error);
-				this.errorMessage = "Login failed.";
+				this.loading = false;
+				
+				switch(error.status) {
+				case 0:
+					this.errorMessage = "Error.ConnectionRefused";
+					break;
+				case 401:
+					this.errorMessage = "Error.InvalidCredentials";
+					break;
+				default:
+					this.errorMessage = "Login failed (Reason: Unknown).";
+					break;
+				}
 			}
 		);
 	}
