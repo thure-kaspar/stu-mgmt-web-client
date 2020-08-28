e6afee6eca3f1b37d2532b44f0c762b79b9ac433
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-08-28 07:57:47 +0200

    Add settings for test environment to env.js
    localhost won't work since parameters are used by the browser.... Added test environment settings, which work on test system and need to be changed anyway in production mode.

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 (function (window) {
 	window.__env = window.__env || {};
 
-	window.__env.API_BASE_PATH = "http://localhost:3000";
-	window.__env.AUTH_BASE_PATH = "http://localhost:8080";
+	window.__env.API_BASE_PATH = "http://147.172.178.30:3000";
+	window.__env.AUTH_BASE_PATH = "http://147.172.178.30:8080";
 
 }(this));
