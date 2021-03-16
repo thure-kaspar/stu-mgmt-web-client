b505ae163171a9067fea6271df6b42cc923b307d
klingebiel <lukas.klingebiel@web.de>
2021-03-16 16:13:02 +0100

    fix(assignment-card): display group warning and student/teaching staff related sections

--- src/app/assignment/components/assignment-card/assignment-card.component.ts
+++ src/app/assignment/components/assignment-card/assignment-card.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { ActivatedRoute, Router } from "@angular/router";
-import { Subject } from "rxjs";
-import { filter, take } from "rxjs/operators";
-import { AssignmentDto, GroupDto, UsersService, AssessmentsService } from "../../../../../api";
+import { Store } from "@ngrx/store";
+import { TranslateService } from "@ngx-translate/core";
+import { Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
+import { AssignmentDto, GroupDto, UsersService } from "../../../../../api";
 import { getRouteParam } from "../../../../../utils/helper";
+import { Course } from "../../../domain/course.model";
 import { Participant } from "../../../domain/participant.model";
 import {
 	ConfirmDialog,
@@ -12,14 +15,13 @@ import {
 } from "../../../shared/components/dialogs/confirm-dialog/confirm-dialog.dialog";
 import { UnsubscribeOnDestroy } from "../../../shared/components/unsubscribe-on-destroy.component";
 import { ToastService } from "../../../shared/services/toast.service";
+import { ParticipantSelectors } from "../../../state/participant";
+import { State } from "../../../state/participant/groups/groups.reducer";
 import {
 	EditAssignmentDialog,
 	EditAssignmentDialogData
 } from "../../dialogs/edit-assignment/edit-assignment.dialog";
 import { AssignmentManagementFacade } from "../../services/assignment-management.facade";
-import { Course } from "../../../domain/course.model";
-import { TranslateService } from "@ngx-translate/core";
-import { ParticipantFacade } from "../../../shared/services/participant.facade";
 
 @Component({
 	selector: "app-assignment-card",
@@ -30,61 +32,53 @@ import { ParticipantFacade } from "../../../shared/services/participant.facade";
 export class AssignmentCardComponent extends UnsubscribeOnDestroy implements OnInit {
 	@Input() assignment: AssignmentDto;
 	@Input() course: Course;
-
-	_participant: Participant;
-	@Input() set participant(p: Participant) {
-		this._participant = p;
-		if (this.studentShouldHaveAGroup(this.assignment, this._participant)) {
-			this.displayGroupOrWarning();
-		}
-	}
-
-	group$ = new Subject<GroupDto>();
+	@Input() participant: Participant;
 
 	/**
 	 * Can be used to display a warning about this assignment, i.e. "You have no group for this assignment."
 	 * Will be translated in the template.
 	 */
 	warning?: string;
+	courseId: string;
+	group$: Observable<GroupDto>;
 
 	typeEnum = AssignmentDto.TypeEnum;
 	stateEnum = AssignmentDto.StateEnum;
 	collaborationEnum = AssignmentDto.CollaborationEnum;
-	courseId: string;
 
 	constructor(
-		private participantFacade: ParticipantFacade,
 		private route: ActivatedRoute,
 		private router: Router,
 		private dialog: MatDialog,
 		private assignmentManagement: AssignmentManagementFacade,
 		private userService: UsersService,
 		private translate: TranslateService,
-		private toast: ToastService
+		private toast: ToastService,
+		private store: Store
 	) {
 		super();
 	}
 
 	ngOnInit(): void {
 		this.courseId = getRouteParam("courseId", this.route);
+		this.group$ = this.store.select(ParticipantSelectors.selectParticipantGroupsState).pipe(
+			tap(state => {
+				if (this.studentHasNoGroup(state)) {
+					if (
+						this.assignment.state === "IN_PROGRESS" &&
+						this.studentShouldHaveAGroup(this.assignment, this.participant)
+					) {
+						this.warning = this.translate.instant("Text.Group.NoGroupForAssignment");
+						this.toast.warning("Text.Group.NoGroupForAssignment", this.assignment.name);
+					}
+				}
+			}),
+			map(state => state.data?.[this.assignment.id])
+		);
 	}
 
-	private displayGroupOrWarning(): void {
-		this.subs.sink = this.participantFacade.assignmentGroups$
-			.pipe(
-				filter(tuples => !!tuples),
-				take(1)
-			)
-			.subscribe(tuples => {
-				const group = tuples.find(a => a.assignment.id === this.assignment.id)?.group;
-
-				if (!group && this.assignment.state === "IN_PROGRESS") {
-					this.warning = this.translate.instant("Text.Group.NoGroupForAssignment");
-					this.toast.warning("Text.Group.NoGroupForAssignment", this.assignment.name);
-				}
-
-				this.group$.next(group);
-			});
+	private studentHasNoGroup(state: State): boolean {
+		return state.hasLoaded && !state.data?.[this.assignment.id];
 	}
 
 	private studentShouldHaveAGroup(assignment: AssignmentDto, participant: Participant): boolean {
@@ -111,7 +105,7 @@ export class AssignmentCardComponent extends UnsubscribeOnDestroy implements OnI
 				if (confirmed) {
 					this.subs.sink = this.assignmentManagement
 						.remove(this.assignment, this.courseId)
-						.subscribe(deleted => {
+						.subscribe(() => {
 							this.toast.success(this.assignment.name, "Message.Removed");
 						});
 				}
@@ -123,7 +117,7 @@ export class AssignmentCardComponent extends UnsubscribeOnDestroy implements OnI
 	 */
 	goToAssessment(): void {
 		this.subs.sink = this.userService
-			.getAssessmentOfUser(this._participant.userId, this.courseId, this.assignment.id)
+			.getAssessmentOfUser(this.participant.userId, this.courseId, this.assignment.id)
 			.subscribe({
 				next: assessment => {
 					this.router.navigate([
