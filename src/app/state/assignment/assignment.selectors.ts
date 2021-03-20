ec0458e30d53615237fdbe92a2d6a78195740cb6
klingebiel <lukas.klingebiel@web.de>
2021-03-20 19:17:51 +0100

    fix(assignment): fix faulty selector

--- src/app/state/assignment/assignment.selectors.ts
+++ src/app/state/assignment/assignment.selectors.ts
@@ -1,10 +1,10 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { adapter, assignmentsFeatureKey } from "./assignment.reducer";
+import { adapter, assignmentsFeatureKey, State } from "./assignment.reducer";
 
-export const selectAssignmentState = createFeatureSelector(assignmentsFeatureKey);
-const { selectEntities, selectAll } = adapter.getSelectors();
+export const selectAssignmentState = createFeatureSelector<State>(assignmentsFeatureKey);
+const { selectAll } = adapter.getSelectors();
 
 export const selectAssignments = createSelector(selectAssignmentState, selectAll);
 
 export const selectAssignment = (assignmentId: string) =>
-	createSelector(selectAssignmentState, selectEntities, assignments => assignments[assignmentId]);
+	createSelector(selectAssignmentState, state => state.entities[assignmentId]);
