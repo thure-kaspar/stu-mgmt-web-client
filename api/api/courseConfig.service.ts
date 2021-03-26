6221af3ae268ab0ecbc7146dd49b196e43e71be8
klingebiel <lukas.klingebiel@web.de>
2021-03-26 16:00:34 +0100

    chore(deps): update api client

--- api/api/courseConfig.service.ts
+++ api/api/courseConfig.service.ts
@@ -374,7 +374,6 @@ export class CourseConfigService {
         }
         // to determine the Accept header
         let httpHeaderAccepts: string[] = [
-            'application/json'
         ];
         const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
         if (httpHeaderAcceptSelected != undefined) {
--- api/model/admissionStatusDto.ts
+++ api/model/admissionStatusDto.ts
@@ -14,6 +14,7 @@ import { RuleCheckResult } from './ruleCheckResult';
 
 export interface AdmissionStatusDto { 
     hasAdmission: boolean;
+    hasAdmissionFromPreviousSemester: boolean;
     results: Array<RuleCheckResult>;
     participant: ParticipantDto;
 }
