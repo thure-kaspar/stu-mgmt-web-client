41c56e6807b1b599af58cf661435232029515bcd
klingebiel <lukas.klingebiel@web.de>
2021-04-20 17:42:57 +0200

    feat(icons): use svg instead of material icon font

---
+++ src/app/shared/components/icon.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+
+@Component({
+	selector: "app-icon",
+	template: `<svg
+		[ngStyle]="{ color: 'red', height: size + 'px', width: size + 'px' }"
+		style="display: inline-block; vertical-align: top; position: relative;"
+	>
+		<use [attr.href]="'assets/icons/sprites.svg#' + name"></use>
+	</svg>`,
+	encapsulation: ViewEncapsulation.None
+})
+export class IconComponent implements OnInit {
+	@Input() name: string;
+	@Input() size = "24";
+
+	constructor() {}
+
+	ngOnInit(): void {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,17 +5,18 @@ import { MatNativeDateModule } from "@angular/material/core";
 import { TranslateModule } from "@ngx-translate/core";
 import { AuthModule } from "../auth/auth.module";
 import { MaterialModule } from "../material/material.module";
+import { AssignmentTypeChipComponent } from "./components/assignment-type-chip/assignment-type-chip.component";
 import { CardComponent } from "./components/card/card.component";
 import { ChipComponent } from "./components/chip/chip.component";
 import { ConfirmDialog } from "./components/dialogs/confirm-dialog/confirm-dialog.dialog";
 import { ExtendedConfirmDialog } from "./components/dialogs/extended-confirm-dialog/extended-confirm-dialog.dialog";
 import { SearchParticipantDialog } from "./components/dialogs/search-participant/search-participant.dialog";
+import { IconComponent } from "./components/icon.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { ParticipantAdmissionStatusComponent } from "./components/participant-admission-status/participant-admission-status.component";
+import { ThumbChipComponent } from "./components/thumb-chip/thumb-chip.component";
 import { Paginator } from "./paginator/paginator.component";
 import { SemesterPipe } from "./pipes/semester.pipe";
-import { AssignmentTypeChipComponent } from "./components/assignment-type-chip/assignment-type-chip.component";
-import { ThumbChipComponent } from "./components/thumb-chip/thumb-chip.component";
-import { ParticipantAdmissionStatusComponent } from "./components/participant-admission-status/participant-admission-status.component";
 
 @NgModule({
 	declarations: [
@@ -27,6 +28,7 @@ import { ParticipantAdmissionStatusComponent } from "./components/participant-ad
 		ExtendedConfirmDialog,
 		CardComponent,
 		ChipComponent,
+		IconComponent,
 		AssignmentTypeChipComponent,
 		ThumbChipComponent,
 		ParticipantAdmissionStatusComponent
@@ -44,6 +46,7 @@ import { ParticipantAdmissionStatusComponent } from "./components/participant-ad
 		SemesterPipe,
 		CardComponent,
 		ChipComponent,
+		IconComponent,
 		AssignmentTypeChipComponent,
 		ThumbChipComponent,
 		ParticipantAdmissionStatusComponent
