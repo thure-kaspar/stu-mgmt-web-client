cefb238f0769e17bc81a618a631892efdccb7d60
klingebiel <lukas.klingebiel@web.de>
2021-08-05 17:26:35 +0200

    refactor: remove unused properties

--- src/app/assignment/components/participant-admission-status-container/participant-admission-status-container.component.ts
+++ src/app/assignment/components/participant-admission-status-container/participant-admission-status-container.component.ts
@@ -1,9 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { combineLatest } from "rxjs";
-import { filter, map } from "rxjs/operators";
-import { AdmissionRuleDto } from "../../../../../api";
-import { CourseSelectors } from "../../../state/course";
 import { ParticipantSelectors } from "../../../state/participant";
 
 @Component({
@@ -11,28 +7,10 @@ import { ParticipantSelectors } from "../../../state/participant";
 	templateUrl: "./participant-admission-status-container.component.html",
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ParticipantAdmissionStatusContainerComponent implements OnInit {
+export class ParticipantAdmissionStatusContainerComponent {
 	admissionStatus$ = this.store.select(
 		ParticipantSelectors.selectParticipantAdmissionStatusState
 	);
 
-	criteria$ = this.store
-		.select(CourseSelectors.selectCourse)
-		.pipe(map(course => course.admissionCriteria));
-
-	vm$ = combineLatest([this.admissionStatus$, this.criteria$]).pipe(
-		filter(([status, criteria]) => !!status.admissionStatus && !!criteria),
-		map(([status, criteria]) =>
-			criteria.rules.map((rule, index) => ({
-				...rule,
-				result: status.admissionStatus.results[index]
-			}))
-		)
-	);
-
-	ruleTypes = AdmissionRuleDto.TypeEnum;
-
 	constructor(private store: Store) {}
-
-	ngOnInit(): void {}
 }
