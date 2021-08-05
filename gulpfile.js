9b2045830acc41d18acb679ff47cf189fb88bb5f
klingebiel <lukas.klingebiel@web.de>
2021-08-05 16:38:50 +0200

    build: move svg sprite generation into separate script

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const svgmin = require("gulp-svgmin");
 const rename = require("gulp-rename");
 const run = require("gulp-run-command").default;
 
+// Combines all .svg-icons to a single file
 gulp.task("svg-sprite", () => {
 	return gulp
 		.src("src/assets/icons/svg/*.svg")
@@ -25,18 +26,3 @@ gulp.task("svg-sprite", () => {
 		.pipe(rename({ basename: "sprites" }))
 		.pipe(gulp.dest("src/assets/icons"));
 });
-
-gulp.task("ng-serve", run("ng serve -o"));
-
-gulp.task("ng-build", run("ng build --prod"));
-
-gulp.task("watch", () =>
-	gulp.watch(["src/assets/icons/svg/*.svg"], gulp.series("svg-sprite"))
-);
-
-gulp.task(
-	"default",
-	gulp.series("svg-sprite", gulp.parallel("ng-serve", "watch"))
-);
-
-gulp.task("build", gulp.series("svg-sprite", "ng-build"));
