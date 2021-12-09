d8febe0e346aab26e1276927b213f69f8482ab86
klingebiel <lukas.klingebiel@web.de>
2021-12-09 14:07:58 +0100

    fix(ui): use tailwind classes for chip

--- apps/client/src/app/shared/components/chip/chip.component.ts
+++ apps/client/src/app/shared/components/chip/chip.component.ts
@@ -3,7 +3,6 @@ import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
 @Component({
 	selector: "app-chip",
 	templateUrl: "./chip.component.html",
-	styleUrls: ["./chip.component.scss"],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChipComponent {
--- apps/client/src/app/shared/components/icon.component.ts
+++ apps/client/src/app/shared/components/icon.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
 @Component({
 	selector: "app-icon",
 	template: `<svg
-		[ngStyle]="{ color: 'red', height: size + 'px', width: size + 'px' }"
+		[ngStyle]="{ color: 'white', height: size + 'px', width: size + 'px' }"
 		style="display: inline-block; margin-top: -2px; vertical-align: middle; position: relative;"
 	>
 		<use [attr.href]="'assets/icons/sprites.svg#' + name"></use>
