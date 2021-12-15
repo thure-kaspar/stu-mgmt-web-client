d9c1794fc3ad96da7f72f84d7adce93058f0fc48
klingebiel <lukas.klingebiel@web.de>
2021-12-15 15:48:41 +0100

    chore: fix incorrect paths in state library configs

--- libs/data-access/state/jest.config.js
+++ libs/data-access/state/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	displayName: "state",
-	preset: "../../jest.preset.js",
+	preset: "../../../jest.preset.js",
 	setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
 	globals: {
 		"ts-jest": {
@@ -8,7 +8,7 @@ module.exports = {
 			stringifyContentPathRegex: "\\.(html|svg)$"
 		}
 	},
-	coverageDirectory: "../../coverage/libs/state",
+	coverageDirectory: "../../coverage/libs/data-access/state",
 	transform: {
 		"^.+\\.(ts|mjs|js|html)$": "jest-preset-angular"
 	},
