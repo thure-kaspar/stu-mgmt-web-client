c719cdb98db02e560f697b60fca84dc90dfae2f8
klingebiel <lukas.klingebiel@web.de>
2021-12-16 16:55:54 +0100

    chore(deps): update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
-const { guessProductionMode } = require("@ngneat/tailwind");
-
-process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
-
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
-	purge: {
-		content: ["./apps/**/*.{html,scss}", "./libs/**/*.{html,scss}"]
-	},
+	content: ["./**/*.html"],
 	darkMode: "class",
 	theme: {
 		extend: {}
