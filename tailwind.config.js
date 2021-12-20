c7c6fba8fd835d2d712e8a0376a77ef5c2d4a979
klingebiel <lukas.klingebiel@web.de>
2021-12-20 16:08:33 +0100

    chore(tailwind): fix incorrect glob pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
-	mode: "jit",
+	//	mode: "jit",
 	purge: {
-		content: ["./apps/client/src/app/*.html", "./libs/**/*.html"]
+		content: ["./apps/client/src/app/**/*.html", "./libs/**/*.html"]
 	},
 	darkMode: "class",
 	theme: {
